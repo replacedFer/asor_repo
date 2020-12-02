3c421723d4285398d356c08ddfad0ffcb3921d4e
fernando <fernando22.fh@gmail.com>
2020-12-02 22:52:51 +0100

    Practica1:  Ejercicio 1 hecho

--- Practica1/ejercicio1.c
+++ Practica1/ejercicio1.c
@@ -1,5 +1,29 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+/*
+compilar 
+
+gcc -c NombreArchivo.c
+gcc -o nombreQueQuieras NombreArchivo.o
+./ nombreQueQuieras 
+
+-------
+gcc -g -o ejercicio1 ejercicio1.c 
+*/
 int main() 
 {
-    setuid(0);
-    return 1;
+    int error;
+
+    error = setuid(0);
+
+    if ( error == -1){
+        printf("Error: %s\n", strerror(errno));
+        perror("Error en setuid");
+        return -1;
+    }
+    return 0;
 }
